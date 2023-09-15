a32f6b0f63ad968b9a77505918297dfc3ed99df0
Jonhamendoza <94213113+Jonhamendoza@users.noreply.github.com>
2023-09-15 01:50:14 -0300

    cambios de prueba

--- main.js
+++ main.js
@@ -1,28 +1,33 @@
 
- function mostrarTexto(){
+    let textoAprocesar = document.getElementById("exampleFormControlTextarea1").value;
+    let textoProcesado = procesarTexto(textoAprocesar);
+
+    
+    function mostrarTexto(){
         var txtProcesado = textoAprocesar;
         if(txtProcesado == ""){
          document.getElementById("contenedorTextoResultado").value= "Ingrese un texto...";
         } else {
             document.getElementById("contenedorTextoResultado").value= textoProcesado;
         }
-        let textoAprocesar = document.getElementById("exampleFormControlTextarea1").value;
-        let textoProcesado = procesarTexto(textoAprocesar);
+        
 
 
-function procesarTexto(input){
-    var txt = [];
-    var clave = ["A", "B", "C"];
-    for( var i = 0; i < input.length; i++){
-        var charCode = input.charCodeAt(i) ^ clave[i % clave.length].charCodeAt(0);
-        txt.push(String.fromCharCode(charCode));
-    }
-    return txt.join("");
 
     }
+    function procesarTexto(input){
+        var txt = [];
+        var clave = ["A", "B", "C"];
+        for( var i = 0; i < input.length; i++){
+            var charCode = input.charCodeAt(i) ^ clave[i % clave.length].charCodeAt(0);
+            txt.push(String.fromCharCode(charCode));
+        }
+        return txt.join("");
+    
+        }
 
-    }
-
+        const btn = document.querySelector("button");
+        btn.addEventListener("click", procesarTexto(textoAprocesar));
 
 
 
